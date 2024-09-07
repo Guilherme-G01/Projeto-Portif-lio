c96c6585a27611beda64e6e4ce24c574c6522941
GUILHERME GRIGOLI <grigoliguilherme00@gmail.com>
2024-09-06 23:40:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,41 @@
+let dados =  
+[
+    {
+        // Nome completo da pessoa
+        nomeCompleto: "Guilherme Grigoli Ribeiro",
+        // URL do perfil no LinkedIn
+        perfilLinkedin: "https://www.linkedin.com/in/guilherme-grigoli-ribeiro-b82012259/"
+    }
+    
+];
+
+const poderosoChefao = [
+  {
+    titulo: "O Poderoso Chefão",
+    sinopse: "A história da ascensão de Vito Corleone ao poder na máfia americana.",
+    genero: "Drama, Crime",
+    diretor: "Francis Ford Coppola",
+    anoLancamento: 1972,
+    elenco: ["Marlon Brando", "Al Pacino"]
+  },
+  {
+    titulo: "O Poderoso Chefão II",
+    sinopse: "Uma história épica que explora a ascensão e queda da família Corleone em duas linhas temporais distintas.",
+    genero: "Drama, Crime",
+    diretor: "Francis Ford Coppola",
+    anoLancamento: 1974,
+    elenco: ["Robert De Niro", "Al Pacino"]
+  },
+  {
+    titulo: "O Poderoso Chefão III",
+    sinopse: "A saga da família Corleone chega ao fim, com Michael Corleone tentando legitimar seus negócios e encontrar um sucessor.",
+    sinopse: "Drama, Crime",
+    diretor: "Francis Ford Coppola",
+    anoLancamento: 1990,
+    elenco: ["Marlon Brando", "Al Pacino"]
+  }
+];
+
+  
+
+    
